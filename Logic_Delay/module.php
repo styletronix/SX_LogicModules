c138f4d25b0d96104e959c109ba6d6c60c03a1de
styletronix <andreas.pross@styletronix.net>
2018-10-06 12:33:09 +0200

    Add files via upload

--- Logic_Delay/module.php
+++ Logic_Delay/module.php
@@ -22,14 +22,6 @@ class Logic_Delay extends IPSModule {
         }
 		
         public function ApplyChanges() {
-			$I_TriggerID = $this->ReadPropertyInteger("I_Trigger");
-			if ($I_TriggerID !> 0){ 
-				$I_TriggerID = IPS_GetParent($_IPS['SELF']); 
-				if (IPS_VariableExists($I_TriggerID)){
-					IPS_SetProperty($_IPS['SELF'], "I_Trigger", $I_TriggerID);
-				}
-			}	
-			
             parent::ApplyChanges();
 				
 			$this->UpdateEvents();			
