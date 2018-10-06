8c4dc8e2679e9bbd5670523cb4cc2430f4e2865b
styletronix <andreas.pross@styletronix.net>
2018-10-06 11:46:48 +0200

    Add files via upload

--- Logic_Delay/module.php
+++ Logic_Delay/module.php
@@ -17,6 +17,8 @@ class Logic_Delay extends IPSModule {
 
             $this->RegisterTimer("Timer_onDelay",0,'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "Timer_onDelay");');	
 			$this->RegisterTimer("Timer_offDelay",0,'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "Timer_offDelay");');	
+			
+			$this->SetBuffer("Status", "off");
         }
 		
         public function ApplyChanges() {
@@ -37,26 +39,45 @@ class Logic_Delay extends IPSModule {
 		}
 		public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
 			if ($Message == 10603){
-				if($SenderID == $this->ReadPropertyInteger("I_Trigger")){
-					$val = GetValue($SenderID);
-					if ($val == true){
-						$this->SetTimerInterval("Timer_offDelay", 0);
-						$this->SetTimerInterval("Timer_onDelay", ($this->ReadPropertyFloat("OnDelay") * 1000) + 1);
+				$I_TriggerID = $this->ReadPropertyInteger("I_Trigger");
+				$I_ResetID = $this->ReadPropertyInteger("I_Reset");
+				
+				$I_Trigger = GetValueBoolean($I_TriggerID);
+				$I_Reset = GetValueBoolean($I_ResetID);
+								
+				if($SenderID == $I_TriggerID and $I_Reset == false){
+					if ($I_Trigger == true){
+						if ($this->GetBuffer("Status") == "off"){
+							$this->SetBuffer("Status", "onDelay");
+							$this->SetTimerInterval("Timer_offDelay", 0);
+							$this->SetTimerInterval("Timer_onDelay", ($this->ReadPropertyFloat("OnDelay") * 1000) + 1);
+						}					
 					}else{
-						$this->SetTimerInterval("Timer_onDelay", 0);
-						$this->SetTimerInterval("Timer_offDelay", ($this->ReadPropertyFloat("OffDelay") * 1000) + 1);						
+						if ($this->GetBuffer("Status") != "offDelay"){
+							$this->SetBuffer("Status", "offDelay");
+							$this->SetTimerInterval("Timer_onDelay", 0);						
+							$this->SetTimerInterval("Timer_offDelay", ($this->ReadPropertyFloat("OffDelay") * 1000) + 1);	
+						}										
 					}
 				}
-				if($SenderID == $this->ReadPropertyInteger("I_Reset")){
-					$val = GetValue($SenderID);
-					if ($val == true){
+				
+				if($SenderID == $I_ResetID){
+					if ($I_Reset == true){
+						$this->SetBuffer("Status", "off");
 						$this->SetTimerInterval("Timer_offDelay", 0);
 						$this->SetTimerInterval("Timer_onDelay", 0);
 						SetValue($this->GetIDForIdent("Output"), false);
-					}				
+					}else if ($I_Trigger == true){
+						if ($this->GetBuffer("Status") == "off"){
+							$this->SetBuffer("Status", "onDelay");
+							$this->SetTimerInterval("Timer_offDelay", 0);
+							$this->SetTimerInterval("Timer_onDelay", ($this->ReadPropertyFloat("OnDelay") * 1000) + 1);
+						}	
+					}					
 				}
 			}
-		}
+		}		
+		
 		public function RequestAction($Ident, $Value) {
     		switch($Ident) {
                 case "TimerCallback":
@@ -73,10 +94,12 @@ class Logic_Delay extends IPSModule {
 			
 			switch($Timer) {
 				case "Timer_onDelay":				
+					$this->SetBuffer("Status", "on");
 					SetValue($this->GetIDForIdent("Output"), true);
 					break;
 					
 				case "Timer_offDelay":
+					$this->SetBuffer("Status", "off");
 					SetValue($this->GetIDForIdent("Output"), false);
 					break;
 					
